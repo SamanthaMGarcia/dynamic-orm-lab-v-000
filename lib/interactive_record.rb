f213f58f76fd051f439c21c360c08315662bc8e5
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-26 13:34:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,9 +28,8 @@ class InteractiveRecord
         end
       values.join(", ")
     end
-  
-    def self.column_names
 
+    def self.column_names
       sql = "pragma table_info('#{table_name}')"
 
       table_info = DB[:conn].execute(sql)
