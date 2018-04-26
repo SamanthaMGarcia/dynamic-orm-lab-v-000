4fccd32a9346f3f626df074050c33008057a2c02
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-26 13:33:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,13 +22,12 @@ class InteractiveRecord
     end
 
     def values_for_insert
-      sql = <<-SQL
-      INSERT INTO table_name
-      WHERE VALUES = ? ?
-      SQL
-
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-
+    values.join(", ")
+  end
     def self.column_names
 
       sql = "pragma table_info('#{table_name}')"
