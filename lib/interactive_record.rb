1575956a4a2441b1e8fe23ed2928595820e6e3cc
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-26 13:35:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,6 @@ class InteractiveRecord
       VALUES (#{values_for_insert})"
       SQL
 
-      
         DB[:conn].execute(sql)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     end
