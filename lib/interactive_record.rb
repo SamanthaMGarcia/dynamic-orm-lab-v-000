2f6288dab37f89f1a09f30ed39fd981a06ecb98f
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-26 13:10:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,8 +17,8 @@ class InteractiveRecord
       self.class.table_name
     end
 
-    def self.column_names_for_insert
-      self.class.column_names
+    def col_names_for_insert
+      self.class.column_names.delete_if {|col| col == "id"}.join(", ")
     end
 
     def self.column_names
