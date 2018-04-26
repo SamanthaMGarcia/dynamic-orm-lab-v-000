56a88857b65639bbe69815794924af12321b71af
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-26 13:38:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,4 +46,9 @@ class InteractiveRecord
     end
       column_names.compact
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 end
